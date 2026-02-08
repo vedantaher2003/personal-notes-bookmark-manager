471e9ca79f39d60c932cf641d8459ce9556fe6b9
Vedant Aher Patil <115220300+vedantaher2003@users.noreply.github.com>
2026-02-09 00:29:22 +0530

    Initialize _app.js with global styles and dark mode

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import "../styles/globals.css";
+import { useEffect } from "react";
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    document.documentElement.classList.add("dark");
+  }, []);
+
+  return <Component {...pageProps} />;
+}
