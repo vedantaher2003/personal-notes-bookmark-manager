9ad9d222a271a9ae0d29e1836d39772cf7c3ca31
Vedant Aher Patil <115220300+vedantaher2003@users.noreply.github.com>
2026-02-09 00:42:09 +0530

    Add authentication controller for user login and registration
    Implement user registration and login functionality with JWT.

---
+++ pages/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+export const register = async (req, res) => {
+  const user = await User.create(req.body);
+  res.json({ token: generateToken(user._id) });
+};
+
+export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user || !(await bcrypt.compare(req.body.password, user.password)))
+    return res.status(401).json({ message: "Invalid credentials" });
+
+  res.json({ token: generateToken(user._id) });
+};
